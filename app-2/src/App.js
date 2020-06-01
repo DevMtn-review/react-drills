c04f2a4f5d6ee0fe43953ae95c73ef1bcfbbb7be
Trey Waller <Trey@treys-mbp.attlocal.net>
2020-06-01 13:16:30 -0500

    app-2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [arr, setArr] = useState(['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-2'>
+        {arr.map(e => <h2>{e}</h2>)}
     </div>
-  );
+  )
 }
-
-export default App;
