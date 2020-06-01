304c1e8bb00837e9dca11f250d7b414c0ce0c913
Trey Waller <Trey@treys-mbp.attlocal.net>
2020-06-01 15:33:33 -0500

    app-4 complete

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+  const login = () => {alert(`Username: ${user} Password: ${pass}`)}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-4'>
+      <input onChange={e => setUser(e.target.value)}/>
+      <input onChange={e => setPass(e.target.value)}/>
+      <button onClick={login}>Login</button>
     </div>
-  );
+  )
 }
-
-export default App;
