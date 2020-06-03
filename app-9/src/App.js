246f7c3fd11876105e51ac9038df8da45844a5a9
Trey Waller <Trey@Treys-MacBook-Pro.local>
2020-06-03 12:21:04 -0500

    app-9 complete

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Routes from './Routes'
+import { Link } from 'react-router-dom'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-9'>
+      <nav className='navbar'>
+        <Link to='/'>Home</Link>
+        <Link to='/signup'>Signup</Link>
+        <Link to='/details'>Details</Link>
+      </nav>
+      {Routes}
     </div>
-  );
+  )
 }
-
-export default App;
